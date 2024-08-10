6054702ddb02cf379a2b8b084dfe6056a8355545
Bearslay <barfunkel@protonmail.com>
2024-08-10 12:47:01 -0400

    added mousestates and a way to emulate buttons

--- bengine/bengine_mouse.hpp
+++ bengine/bengine_mouse.hpp
@@ -1,17 +1,21 @@
 #ifndef BENGINE_MOUSE_hpp
 #define BENGINE_MOUSE_hpp
 
-#include <SDL2/SDL_mouse.h>
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <cmath>
+
+#include "bengine_window.hpp"
 
 namespace bengine {
-    enum {
-        MOUSE1 = 0,
-        MOUSE2 = 1,
+    typedef enum {
+        MOUSE1 = 1,
+        MOUSE2 = 3,
         MOUSE3 = 2,
         MOUSE4 = 4,
         MOUSE5 = 5,
-        LEFT_MOUSE_BUTTON = 0,
-        RIGHT_MOUSE_BUTTON = 1,
+        LEFT_MOUSE_BUTTON = 1,
+        RIGHT_MOUSE_BUTTON = 3,
         MIDDLE_MOUSE_BUTTON = 2,
         BACKWARD_MOUSE_BUTTON = 4,
         FORWARD_MOUSE_BUTTON = 5
@@ -19,73 +23,357 @@ namespace bengine {
 
     class genericMouseState {
         protected:
-            SDL_Point pos = {0, 0};
+            Uint16 x;
+            Uint16 y;
             bool motion = false;
             bool buttons[5] = {false, false, false, false, false};
 
         public:
-            genericMouseState() {
+            genericMouseState() {}
+            ~genericMouseState() {}
 
+            virtual void updateMotion(const SDL_Event &event) = 0;
+
+            void pressButton(const SDL_Event &event) {
+                this->buttons[event.button.button - 1] = true;
             }
-            ~genericMouseState() {
-
+            void pressButton(const Uint8 &button) {
+                if (button > 0 && button < 6) {
+                    this->buttons[button] = true;
+                }
             }
-
-            virtual void update();
+            void releaseButton(const SDL_Event &event) {
+                this->buttons[event.button.button - 1] = false;
+            }
+            void releaseButton(const Uint8 &button) {
+                if (button > 0 && button < 6) {
+                    this->buttons[button] = false;
+                }
+            }
+            void update(const SDL_Event &event) {
+                switch (event.type) {
+                    case SDL_MOUSEMOTION:
+                        this->updateMotion(event);
+                        break;
+                    case SDL_MOUSEBUTTONDOWN:
+                        this->pressButton(event);
+                        break;
+                    case SDL_MOUSEBUTTONUP:
+                        this->releaseButton(event);
+                        break;
+                }
+            }
 
             Uint16 posx() const {
-                return this->pos.x;
+                return this->x;
             }
             Uint16 posy() const {
-                return this->pos.y;
+                return this->y;
             }
             bool moved() const {
                 return this->motion;
             }
             bool pressed(const Uint8 &button) const {
-                if (button > 4) {
+                if (button > 5 || button < 1) {
                     return false;
                 }
-                return this->buttons[button];
+                return this->buttons[button - 1];
             }
     };
 
-    class normalMouseState {
-        private:
-            SDL_Point pos;
-            bool motion = false;
+    class normalMouseState : public genericMouseState {
+        public:
+            normalMouseState() {}
+            ~normalMouseState() {}
 
-            bool buttons[5] = {false, false, false, false, false};
+            void updateMotion(const SDL_Event &event) {
+                this->motion = true;
+                this->x = event.motion.x;
+                this->y = event.motion.y;
+            }
+    };
+
+    class relativeMouseState : public genericMouseState {
+        private:
+            double sensitivity = 10;
+
+            bool released = true;
+            bool captured = false;
+
+        public:
+            relativeMouseState() {}
+            ~relativeMouseState() {}
+
+            void updateMotion(const SDL_Event &event) {
+                this->motion = true;
+                this->x = event.motion.xrel;
+                this->y = event.motion.yrel;
+
+                if (this->released) {
+                    this->released = false;
+                    this->x = this->y = 0;
+                }
+            }
+
+            bool isCaptured() const {
+                return this->captured;
+            }
+            bool capture() {
+                const bool output = this->captured;
+                if (SDL_SetRelativeMouseMode(SDL_TRUE) != 0) {
+                    std::cout << "Could not enter relative mouse mode\nERROR [" << SDL_GetTicks() << "]: " << SDL_GetError() << "\n";
+                }
+                this->x = this->y = 0;
+                this->captured = true;
+                return output;
+            }
+            bool release() {
+                const bool output = this->captured;
+                if (SDL_SetRelativeMouseMode(SDL_FALSE) != 0) {
+                    std::cout << "Could not exit relative mouse mode\nERROR [" << SDL_GetTicks() << "]: " << SDL_GetError() << "\n";
+                }
+                this->x = this->y = 0;
+                this->captured = false;
+                this->released = true;
+                return output;
+            }
+            bool release(bengine::window &win) {
+                const bool output = this->captured;
+                if (SDL_SetRelativeMouseMode(SDL_FALSE) != 0) {
+                    std::cout << "Could not exit relative mouse mode\nERROR [" << SDL_GetTicks() << "]: " << SDL_GetError() << "\n";
+                }
+                win.centerMouse();
+                this->x = this->y = 0;
+                this->captured = false;
+                this->released = true;
+                return output;
+            }
+
+            double getSensitivity() const {
+                return this->sensitivity;
+            }
+            double setSensitivity(const double &sensitivity) {
+                const double output = this->sensitivity;
+                this->sensitivity = sensitivity;
+                return output;
+            }
+    };
+
+    class clickRectangle {
+        protected:
+            Uint16 x1 = 0;
+            Uint16 y1 = 0;
+            Uint16 x2 = 0;
+            Uint16 y2 = 0;
+
+        public:
+            clickRectangle(const Uint16 x1 = 0, const Uint16 y1 = 0, const Uint16 x2 = 0, const Uint16 y2 = 0){
+                this->setAreaCorners(x1, y1, x2, y2);
+            }
+            ~clickRectangle() {}
+
+            Uint16 getX1() const {
+                return this->x1;
+            }
+            Uint16 getY1() const {
+                return this->y1;
+            }
+            Uint16 getX2() const {
+                return this->x2;
+            }
+            Uint16 getY2() const {
+                return this->y2;
+            }
+            Uint16 setX1(const Uint16 &x) {
+                const Uint16 output = this->x1;
+                this->x1 = x;
+                return output;
+            }
+            Uint16 setY1(const Uint16 &y) {
+                const Uint16 output = this->y1;
+                this->y1 = y;
+                return output;
+            }
+            Uint16 setX2(const Uint16 &x) {
+                const Uint16 output = this->x2;
+                this->x2 = x;
+                return output;
+            }
+            Uint16 setY2(const Uint16 &y) {
+                const Uint16 output = this->y2;
+                this->y2 = y;
+                return output;
+            }
+            void setAreaCorners(const Uint16 x1, const Uint16 y1, const Uint16 x2, const Uint16 y2) {
+                if (x1 < x2) {
+                    this->x1 = x1;
+                    this->x2 = x2;
+                } else {
+                    this->x1 = x2;
+                    this->x2 = x1;
+                }
+                if (y1 < y2) {
+                    this->y1 = y1;
+                    this->y2 = y2;
+                } else {
+                    this->y1 = y2;
+                    this->y2 = y1;
+                }
+            }
+
+            Uint16 getX() const {
+                return this->x1;
+            }
+            Uint16 getY() const {
+                return this->y1;
+            }
+            Uint16 getW() const {
+                return this->x2 - this->x1;
+            }
+            Uint16 getH() const {
+                return this->y2 - this->y1;
+            }
+            Uint16 setX(const Uint16 &x) {
+                return this->setX1(x);
+            }
+            Uint16 setY(const Uint16 &y) {
+                return this->setY1(y);
+            }
+            Uint16 setW(const Uint16 &w) {
+                const Uint16 output = this->getW();
+                this->x2 = this->x1 + w;
+                return output;
+            }
+            Uint16 setH(const Uint16 &h) {
+                const Uint16 output = this->getH();
+                this->y2 = this->y1 + h;
+                return output;
+            }
+            void setAreaRect(const Uint16 x, const Uint16 y, const Uint16 &w, const Uint16 &h) {
+                this->x1 = x;
+                this->x2 = x + w;
+                this->y1 = y;
+                this->y2 = y + h;
+            }
+
+            bool checkPos(const normalMouseState &mouseState) const {
+                if (this->getW() == 0 || this->getH() == 0) {
+                    return false;
+                }
+                return mouseState.posx() >= this->x1 && mouseState.posy() >= this->y1 && mouseState.posx() <= this->x2 && mouseState.posy() <= this->y2;
+            }
+            bool checkButton(const normalMouseState &mouseState, const Uint8 &button) const {
+                if (!mouseState.pressed(button)) {
+                    return false;
+                }
+                return this->checkPos(mouseState);
+            }
+    };
+
+    class clickCircle {
+        protected:
+            Uint16 x = 0;
+            Uint16 y = 0;
+            Uint16 radius = 0;
+
+        public:
+            clickCircle(const Uint16 &x = 0, const Uint16 &y = 0, const Uint16 &radius = 0) : x(x), y(y), radius(radius) {}
+            ~clickCircle() {}
+
+            Uint16 getX() const {
+                return this->x;
+            }
+            Uint16 getY() const {
+                return this->y;
+            }
+            Uint16 getRadius() const {
+                return this->radius;
+            }
+            Uint16 setX(const Uint16 &x) {
+                const Uint16 output = this->x;
+                this->x = x;
+                return output;
+            }
+            Uint16 setY(const Uint16 &y) {
+                const Uint16 output = this->y;
+                this->y = y;
+                return output;
+            }
+            Uint16 setRadius(const Uint16 &radius) {
+                const Uint16 output = this->radius;
+                this->radius = radius;
+                return output;
+            }
+
+            bool checkPos(const normalMouseState &mouseState) const {
+                if (this->radius == 0) {
+                    return false;
+                }
+                // First a square is checked to save on some math in the most common cases, then the circle is checked if the mouse is within the matching square
+                if (mouseState.posx() >= this->x - radius && mouseState.posx() <= this->x + radius && mouseState.posy() >= this->y - radius && mouseState.posy() <= this->y + radius) {
+                    const double root = std::round(std::sqrt(this->radius * this->radius - (mouseState.posx() - this->x) * (mouseState.posx() - this->x)));
+                    return mouseState.posy() < this->y + root && mouseState.posy() > this->y - root;
+                }
+                return false;
+            }
+            bool checkButton(const normalMouseState &mouseState, const Uint8 &button) const {
+                if (!mouseState.pressed(button)) {
+                    return false;
+                }
+                return this->checkPos(mouseState);
+            }
+    };
+
+    class clickMatrix : public clickRectangle {
+        protected:
+            Uint16 rows = 1;
+            Uint16 cols = 1;
         
         public:
-            normalMouseState() {
+            clickMatrix(const Uint16 x1 = 0, const Uint16 y1 = 0, const Uint16 x2 = 0, const Uint16 y2 = 0, const Uint16 &rows = 1, const Uint16 &cols = 1) : clickRectangle(x1, y1, x2, y2) {
+                this->setRows(rows);
+                this->setCols(cols);
+            }
+            ~clickMatrix() {}
 
+            Uint16 getRows() const {
+                return this->rows;
             }
-
-            void update() {
-                this->motion = true;
-                SDL_GetMouseState(&this->pos.x, &this->pos.y);
+            Uint16 getCols() const {
+                return this->cols;
             }
-
-            Uint16 posx() const {
-                return this->pos.x;
-            }
-            Uint16 posy() const {
-                return this->pos.y;
-            }
-            bool moved() const {
-                return this->motion;
-            }
-            bool pressed(const Uint8 &button) const {
-                if (button > 4) {
-                    return false;
+            Uint16 setRows(const Uint16 &rows) {
+                const Uint16 output = this->rows;
+                if (rows > this->getH()) {
+                    this->rows = this->getH();
+                } else {
+                    this->rows = rows;
                 }
-                return this->buttons[button];
+                return output;
             }
-    };
+            Uint16 setCols(const Uint16 &cols) {
+                const Uint16 output = this->cols;
+                if (cols > this->getW()) {
+                    this->cols = this->getW();
+                } else {
+                    this->cols = cols;
+                }
+                return output;
+            }
 
-    class relativeMouseState {
-
+            Uint32 checkPos(const normalMouseState &mouseState) const {
+                if (this->getW() == 0 || this->getH() == 0) {
+                    return UINT32_MAX;
+                }
+                if (mouseState.posx() >= this->x1 && mouseState.posx() <= this->x2 && mouseState.posy() >= this->y1 && mouseState.posy() <= this->y2) {
+                    return (mouseState.posy() == this->y2 ? this->rows - 1 : (mouseState.posy() - this->y1) / (this->getH() / this->rows)) * this->cols + (mouseState.posx() == this->x2 ? this->cols - 1 : (mouseState.posx() - this->x1) / (this->getW() / this->cols));
+                }
+                return UINT32_MAX;
+            }
+            Uint32 checkButton(const normalMouseState &mouseState, const Uint8 &button) const {
+                const Uint32 pos = this->checkPos(mouseState);
+                return mouseState.pressed(button) ? pos : UINT32_MAX;
+            }
     };
 }
 
