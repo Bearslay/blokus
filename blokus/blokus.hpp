dba6002d3836f5a44069ab10c86c102c43958208
Bearslay <barfunkel@protonmail.com>
2024-08-10 13:01:47 -0400

    added a catch-all file for blokus inclusion (to replace the old, massive blokus file)

---
+++ blokus/blokus.hpp
@@ -0,0 +1,9 @@
+#ifndef BLOKUS_hpp
+#define BLOKUS_hpp
+
+#include "blokus_polyominoes.hpp"
+#include "blokus_piece.hpp"
+#include "blokus_player.hpp"
+#include "blokus_game.hpp"
+
+#endif /* BLOKUS_hpp */
