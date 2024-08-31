5da012dd4e2e50082601990f459d3b6fad787542
Bearslay <barfunkel@protonmail.com>
2024-08-30 22:55:12 -0400

    added function/class hints

--- blokus/blokus_polyominoes.hpp
+++ blokus/blokus_polyominoes.hpp
@@ -23,70 +23,35 @@
  * [polyomino c: grid line d]
 **/
 
-// #define ALIGN_LEFT 0
-// #define ALIGN_CENTER 1
-// #define ALIGN_RIGHT 2
-
-// class paddedGrid {
-//     public:
-//         unsigned int calcUsableLength(const unsigned int &length, const unsigned short &cellAmount, const unsigned short &gapDim) {
-//             return length - gapDim * (cellAmount - 1);
-//         }
-//         unsigned short calcCellDim(const unsigned int &length, const unsigned short &cellAmount, const unsigned short &gapDim) {
-//             return this->calcUsableLength(length, cellAmount, gapDim) / cellAmount;
-//         }
-//         unsigned short calcOffset(const unsigned int &length, const unsigned short &cellAmount, const unsigned short &gapDim, const unsigned char &alignment = ALIGN_CENTER) {
-//             switch (alignment) {
-//                 case ALIGN_LEFT:
-//                     return 0;
-//                 default:
-//                 case ALIGN_CENTER:
-//                     return (this->calcUsableLength(length, cellAmount, gapDim) - this->calcCellDim(length, cellAmount, gapDim) * cellAmount) / 2;
-//                 case ALIGN_RIGHT:
-//                     return this->calcUsableLength(length, cellAmount, gapDim) - this->calcCellDim(length, cellAmount, gapDim) * cellAmount;
-//             }
-//         }
-//         unsigned short calcOffset(const unsigned int &usableLength, const unsigned short &cellAmount, const unsigned short &cellDim, const unsigned char &alignment = ALIGN_CENTER) {
-//             switch (alignment) {
-//                 case ALIGN_LEFT:
-//                     return 0;
-//                 default:
-//                 case ALIGN_CENTER:
-//                     return (usableLength - cellDim * cellAmount) / 2;
-//                 case ALIGN_RIGHT:
-//                     return usableLength - cellDim * cellAmount;
-//             }
-//         }
-
-//         unsigned short calcSquareCellDim(const unsigned int &width, const unsigned int &height, const unsigned short &columns, const unsigned short &rows, const unsigned short &gapWidth, const unsigned short &gapHeight) {
-//             unsigned short width = this->calcCellDim(width, columns, gapWidth);
-//             unsigned short height = this->calcCellDim(height, rows, gapHeight);
-//             return width < height ? width : height;
-//         }
-// };
-
 namespace blokus {
+    /// @brief The amount of polyominoes in each set
     const unsigned short polyominoAmounts[6] = {21, 35, 108, 369, 1285, 4655};
+    /// @brief The amount of tiles each polyomino within each set takes up (the amount of cells that makes up a given polyomino)
     const unsigned char polyominoTiles[6] = {0, 6, 7, 8, 9, 10};
+    /// @brief The total amount of tiles each polyomino set takes up (pretty much blokus::polyominoAmounts[x] * blokus::polyominoTiles[x])
     const unsigned short polyominoTileTotals[6] = {89, 210, 756, 2952, 11565, 46550};
 
     typedef std::size_t polyType;
     typedef std::size_t polyominoType;
     typedef enum {
-        POLYTYPE_BASE = 0,
-        POLYTYPE_HEXOMINO = 1,
-        POLYTYPE_HEPTOMINO = 2,
-        POLYTYPE_OCTOMINO = 3,
-        POLYTYPE_NONOMINO = 4,
-        POLYTYPE_DECOMINO = 5,
-        POLYTYPE_HEX = 1,
-        POLYTYPE_HEPT = 2,
-        POLYTYPE_OCT = 3,
-        POLYTYPE_NON = 4,
-        POLYTYPE_DEC = 5,
-        POLYTYPE_SENTINAL = -1
+        POLYTYPE_BASE = 0,             // Monominoes, Dominoes, Triominoes, Tetrominoes, Pentominoes
+        POLYTYPE_HEXOMINO = 1,         // Hexominoes
+        POLYTYPE_HEPTOMINO = 2,        // Heptominoes
+        POLYTYPE_OCTOMINO = 3,         // Octominoes
+        POLYTYPE_NONOMINO = 4,         // Nonominoes
+        POLYTYPE_DECOMINO = 5,         // Decominoes
+        POLYTYPE_HEX = 1,              // Hexominoes
+        POLYTYPE_HEPT = 2,             // Heptominoes
+        POLYTYPE_OCT = 3,              // Octominoes
+        POLYTYPE_NON = 4,              // Nonominoes
+        POLYTYPE_DEC = 5,              // Decominoes
+        POLYTYPE_SENTINAL = UINT64_MAX // A sentinal value for polyomino types
     } polyominoTypes;
 
+    /** Load a text file containing polyomino information into a 3-dimensional std::vector
+     * @param filepath The path to the file to read from
+     * @returns A 3-dimensional std::vector containing the information for a list of polyominoes
+     */
     std::vector<std::vector<std::vector<bool>>> readPolyominoFile(const char* filepath) {
         std::vector<std::vector<std::vector<bool>>> output;
         
@@ -139,32 +104,49 @@ namespace blokus {
         return output;
     }
 
-    std::vector<std::vector<std::vector<bool>>> enlargePolyominoList(const std::vector<std::vector<std::vector<bool>>> &input, const unsigned char &increase = 1) {
-        std::vector<std::vector<std::vector<bool>>> output = input;
+    /** Increase the dimensions of a polyomino grid by an amount while keeping the original grid pseudo-centered
+     * @param input The inputted polyomino grid to enlarge
+     * @param cycles The amount of times to increase the grid size
+     * @returns The enlarged polyomino grid
+     */
+    std::vector<std::vector<bool>> enlargePolyominoGrid(const std::vector<std::vector<bool>> &input, const unsigned char &cycles = 1) {
+        std::vector<std::vector<bool>> output = input;
 
-        for (unsigned char i = 0; i < increase; i++) {
-            const unsigned char size = output[0].size();
+        for (unsigned char i = 0; i < cycles; i++) {
+            // Add a new row
+            output.emplace(output.begin() + ((output.size() % 2 == 0) ? 0 : output.size()));
 
-            for (unsigned char j = 0; j < output.size(); j++) {
-                // Add a new row
-                output[j].emplace(output[j].begin() + ((size % 2 == 0) ? 0 : size));
+            // Fill new row with false
+            for (unsigned char j = 0; j < output.size() + 1; j++) {
+                output[(output.size() % 2 == 0) ? 0 : output.size()].emplace_back(false);
+            }
 
-                // Fill new row with false
-                for (unsigned char k = 0; k < size + 1; k++) {
-                    output[j][(size % 2 == 0) ? 0 : size].emplace_back(false);
-                }
-
-                // Add a new column
-                for (unsigned char k = ((size % 2 == 0) ? 1 : 0); k < size + ((size % 2 == 0) ? 1 : 0); k++) {
-                    output[j][k].emplace(output[j][k].begin() + ((size % 2 == 0) ? 0 : size), false);
-                }
+            // Add a new column
+            for (unsigned char j = ((output.size() % 2 == 0) ? 1 : 0); j < output.size() + ((output.size() % 2 == 0) ? 1 : 0); j++) {
+                output[j].emplace(output[j].begin() + ((output.size() % 2 == 0) ? 0 : output.size()), false);
             }
         }
 
         return output;
     }
 
-    std::vector<std::vector<std::vector<std::vector<bool>>>> rawPolyominoData = {
+    /** Increase the dimensions of a list of polyomino grids by an amount while keeping the original grids pseudo-centered
+     * @param input The inputted list of polyomino grids to enlarge
+     * @param cycles The amount of times to increase all of the grids' size
+     * @returns The list of enlarged polyomino grids
+     */
+    std::vector<std::vector<std::vector<bool>>> enlargePolyominoList(const std::vector<std::vector<std::vector<bool>>> &input, const unsigned char &cycles = 1) {
+        std::vector<std::vector<std::vector<bool>>> output;
+
+        for (unsigned char i = 0; i < output.size(); i++) {
+            output.emplace_back(blokus::enlargePolyominoGrid(input.at(i), cycles));
+        }
+
+        return output;
+    }
+
+    /// @brief A list of lists of grids that represent the cell data of each polyomino
+    const std::vector<std::vector<std::vector<std::vector<bool>>>> rawPolyominoData = {
         blokus::readPolyominoFile("dev/polyominoes/base.txt"),
         blokus::readPolyominoFile("dev/polyominoes/hexominoes.txt"),
         blokus::readPolyominoFile("dev/polyominoes/heptominoes.txt"),
